3bf8fbcde7454cf5a37042a12d7c01a70c470796
demansou <demansou@gmail.com>
2017-02-16 03:25:59 -0600

    STARTED BRANCH FOR CELERY TASK QUEUE

--- app.py
+++ app.py
@@ -39,10 +39,8 @@ app.config['CELERY_BROKER_URL'] = 'mongodb://db_admin:dbpass@127.0.0.1:27017/cel
 app.config['CELERY_IMPORTS'] = ("helpers.motif_analysis", "helpers.analyze_sequence",)
 # ONLY NECESSARY FOR STORING RESULTS (RESULTS STORED IN WEB_QUERIES COLLECTION)
 # app.config['CELERY_RESULT_BACKEND'] = 'mongodb://db_admin:dbpass@127.0.0.1:27017/celery_task_results'
-# celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-# celery.conf.update(app.config)
-celery = Celery()
-celery.init_app(app)
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 
 @app.route('/', methods=['GET'])
@@ -339,7 +337,7 @@ def results():
 
     # start analysis
     # SET TO DELAY WHEN RUNNING IN LINUX
-    helpers.motif_analysis.delay(json.dumps(query, default=json_util.default))
+    motif_analysis.delay(json.dumps(query, default=json_util.default))
 
     # render template with data for stats fields
     return render_template('/results/index.html', motifs=query['motifs_as_string'],
@@ -372,6 +370,153 @@ def get_file():
     file_name = ''.join([request.cookies.get('query_id'), '.csv'])
     file_path = os.path.join(os.getcwd(), 'downloads', file_name)
     return send_file(file_path, attachment_filename=file_name, as_attachment=True, mimetype='text/csv')
+
+
+@celery.task()
+def motif_analysis(query):
+    """
+    Asynchronous query analysis via Celery
+    :param query:
+    :return:
+    """
+    query = json.loads(query)
+    with app.app_context():
+        # create shorter variables for motif frequency and frame size
+        motif_frequency = query['motif_frequency']
+        motif_frame_size = query['motif_frame_size']
+
+        # create list with motifs
+        motif_list = []
+        for motif_id in query['motif_list']:
+            motif_query = mongo.db.motif.find_one({'_id': motif_id})
+            motif = motif_query['sequence_motif']
+            motif_list.append(motif)
+
+        # create list with collections
+        collection_list = []
+        for collection_id in query['collection_list']:
+            collection_query = mongo.db.collection.find_one({'_id': collection_id})
+            collection = collection_query['collection']
+            collection_list.append(collection)
+
+        # iterate through each sequence in each collection and do analysis
+        # does not add document to CSV or MongoDB if error encountered
+        for collection in collection_list:
+            for sequence in collection:
+                analyze_sequence.delay(query, sequence, motif_list, motif_frequency, motif_frame_size)
+
+        # update `query` document with `done`
+        mongo.db.query.update({
+            '_id': query['_id']
+        }, {
+            '$set': {
+                'done': True,
+            }
+        })
+
+
+@celery.task()
+def analyze_sequence(query, sequence, motif_list, motif_frequency, motif_frame_size):
+    """
+    Performs sequence analysis on a single BioPython Sequence object using
+    motif parameters
+    :param query:
+    :param sequence:
+    :param motif_list:
+    :param motif_frequency:
+    :param motif_frame_size:
+    :return:
+    """
+    # ensures `sequence` param is dictionary
+    if not isinstance(sequence, dict):
+        return False
+
+    # ensures each `sequence` dictionary value is a string
+    for key, value in sequence.items():
+        if not isinstance(value, str):
+            return False
+
+    # ensures that `motif_list` param is list
+    if not isinstance(motif_list, list):
+        return False
+
+    # ensures that `motif_list` param list is not empty
+    if len(motif_list) == 0:
+        return False
+
+    # ensures that each item in `motif_list` param list is a string
+    for motif in motif_list:
+        if not isinstance(motif, str):
+            return False
+
+    # ensures that `motif_frequency` param is an integer
+    if not isinstance(motif_frequency, int):
+        return False
+
+    # ensures that `motif_frequency` param falls within a range
+    if motif_frequency < 2 or motif_frequency > 10:
+        return False
+
+    # ensures that `motif_frame_size` param is an integer
+    if not isinstance( motif_frame_size, int):
+        return False
+
+    # ensures that `motif_frame_size` param falls within a range
+    if motif_frame_size < 10 or motif_frame_size > 1000:
+        return False
+
+    result_list = []
+    motif_boolean = False
+    for motif in motif_list:
+        # get all motif matches as list
+        raw_match_list = helpers.Private.regex_find_iter(motif, sequence['sequence'])
+
+        # filter out sequences which don't have enough motifs in overall sequence
+        motif_filter_list = []
+        if len(raw_match_list) >= motif_frequency:
+            # print('%s' % raw_match_list)
+
+            for index, match in enumerate(raw_match_list):
+                temp_list = []
+
+                # get motif frame
+                substr_start = match['span'][0]
+                substr_end = substr_start + motif_frame_size
+
+                # start temp list
+                temp_list.append(match)
+
+                # start iterating at next index of `raw_match_list`
+                i = index + 1
+                while i < len(raw_match_list):
+                    if raw_match_list[i]['span'][0] <= substr_end:
+                        temp_list.append(raw_match_list[i])
+                    i += 1
+
+                # if `temp_list` is gte than `motif_frequency`, push
+                if len(temp_list) >= motif_frequency:
+                    # print('MATCH: %s' % temp_list)
+                    motif_filter_list.append(temp_list)
+                    motif_boolean = True
+
+        # for match in match_list compile sequence result dictionary
+        sequence_result = {
+            'motif': motif,
+            'raw_data': raw_match_list,
+            'motif_data': motif_filter_list,
+        }
+        result_list.append(sequence_result)
+
+    # append result to csv and database
+    write_result = helpers.write_results_to_csv(query, sequence, result_list)
+    if write_result is not False:
+        mongo.db.result.insert_one({
+            'query_id': query['_id'],
+            'sequence': sequence,
+            'analysis': result_list,
+            'has_motif': motif_boolean,
+            'datetime_added': datetime.utcnow(),
+        })
 
 
 if __name__ == '__main__':
