abbb060497be59d98e10f6ecd9634d9a77033cbd
demansou <demansou@gmail.com>
2017-02-19 03:49:05 -0600

    DONE MUST TEST CELERY TASK  QUEUE

--- motif_analyzer/celery_tasks.py
+++ motif_analyzer/celery_tasks.py
@@ -5,7 +5,7 @@ from bson import ObjectId
 import re
 
 
-@celery.task(name='tasks.analyze_sequence')
+@celery.task(name='app.celery.analyze_sequence')
 def analyze_sequence(query_id, sequence_description, sequence, motif_list, motif_frequency, motif_frame_size, user):
     """
     Analyzes single sequence and inserts
