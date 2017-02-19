39d5108850fed24b71f40edb57dcca13688dd216
demansou <demansou@gmail.com>
2017-02-19 03:47:39 -0600

    DONE MUST TEST CELERY TASK  QUEUE

--- motif_analyzer/__init__.py
+++ motif_analyzer/__init__.py
@@ -14,7 +14,6 @@ app.config.update(
     MONGO_USERNAME='db_admin',
     MONGO_PASSWORD='dbpass',
     CELERY_BROKER_URL='mongodb://db_admin:dbpass@127.0.0.1:27017/celery',
-    CELERY_IMPORTS=('app.celery_tasks',),
 )
 
 # MongoDB Setup
