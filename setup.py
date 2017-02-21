b47793fb00252da37074c9022ecb461492ddc35f
Daniel Mansour <demansou@users.noreply.github.com>
2017-02-21 01:13:33 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='motif_analyzer',
     version='0.1.0',
-    license='Apache-2.0'
+    license='AGPL-3.0+'
     author='Daniel Mansour',
     author_email='demansou@gmail.com',
     download_url='https://github.com/demansou/flask_motif_analyzer/',
