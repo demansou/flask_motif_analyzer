bfd501b7a1b7f975d2f2f8ee7b61ed3642189629
Daniel Mansour <demansou@users.noreply.github.com>
2017-02-21 01:04:18 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,28 +2,13 @@ from setuptools import setup
 
 setup(
     name='motif_analyzer',
+    version='0.1.0',
+    license='Apache-2.0'
+    author='Daniel Mansour',
+    author_email='demansou@gmail.com',
+    download_url='https://github.com/demansou/flask_motif_analyzer/',
     packages=[
         'motif_analyzer',
     ],
     include_package_data=True,
-    install_requires=[
-        'Flask>=0.12',
-        'Flask-PyMongo>=0.4.1',
-        'Jinja2>=2.9.4',
-        'MarkupSafe>=0.23',
-        'Werkzeug>=0.11.15',
-        'amqp>=2.1.4',
-        'billiard>=3.5.0.2',
-        'biopython>=1.68',
-        'celery>=4.0.2',
-        'click>=6.7',
-        'itsdangerous>=0.24',
-        'kombu>=4.0.2',
-        'pip>=9.0.1',
-        'pymongo>=3.4.0',
-        'pytz>=2016.10',
-        'setuptools>=18.1',
-        'six>=1.10.0',
-        'vine>=1.1.3',
-    ],
 )
